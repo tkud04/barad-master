2201b8fc6ab3bf0dfc2146eda4e21c506a9aa225
tkud04 <32499735+tkud04@users.noreply.github.com>
2018-09-12 17:04:38 +0100

    Update shuffle

--- resources/views/shuffle.blade.php
+++ resources/views/shuffle.blade.php
@@ -25,58 +25,58 @@
 			<div id="filter-content" class="row no-gutters mt-70">
 				<div class="mix category-1 col-lg-3 col-md-4 col-sm-6 single-filter-content content-1" data-myorder="1">
 					<div class="overlay overlay-bg-content d-flex align-items-center justify-content-center flex-column">
-						<p class="text-white">Media Coverage</p>
+						<p class="text-white">Barad Innovations</p>
 						<div class="line"></div>
-						<h5 class="text-white text-uppercase">Weddings & Events</h5>
+						<h5 class="text-white text-uppercase">We create lasting memories</h5>
 					</div>
 				</div>
-				<div class="mix category-1 category-3 col-lg-3 col-md-4 col-sm-6 single-filter-content content-2" data-myorder="2">
+				<div class="mix category-1 category-2 col-lg-3 col-md-4 col-sm-6 single-filter-content content-2" data-myorder="2">
 					<div class="overlay overlay-bg-content d-flex align-items-center justify-content-center flex-column">
-						<p class="text-white">Media Coverage</p>
+						<p class="text-white">Barad Innovations</p>
 						<div class="line"></div>
-						<h5 class="text-white text-uppercase">Weddings & Events</h5>
+						<h5 class="text-white text-uppercase">We create lasting memories</h5>
 					</div>
 				</div>
-				<div class="mix category-1 category-3 col-lg-3 col-md-4 col-sm-6 single-filter-content content-3" data-myorder="3">
+				<div class="mix category-1 category-3 category-4 col-lg-3 col-md-4 col-sm-6 single-filter-content content-3" data-myorder="3">
 					<div class="overlay overlay-bg-content d-flex align-items-center justify-content-center flex-column">
-						<p class="text-white">Media Coverage</p>
+						<p class="text-white">Barad Innovations</p>
 						<div class="line"></div>
-						<h5 class="text-white text-uppercase">Weddings & Events</h5>
+						<h5 class="text-white text-uppercase">We create lasting memories</h5>
 					</div>
 				</div>
-				<div class="mix category-2 category-3 category-4 col-lg-3 col-md-4 col-sm-6 single-filter-content content-4" data-myorder="4">
+				<div class="mix category-1 category-2 category-3 col-lg-3 col-md-4 col-sm-6 single-filter-content content-4" data-myorder="4">
 					<div class="overlay overlay-bg-content d-flex align-items-center justify-content-center flex-column">
-						<p class="text-white">Media Coverage</p>
+						<p class="text-white">Barad Innovations</p>
 						<div class="line"></div>
-						<h5 class="text-white text-uppercase">Weddings & Events</h5>
+						<h5 class="text-white text-uppercase">We create lasting memories</h5>
 					</div>
 				</div>
 				<div class="mix category-1 category-4 category-3 col-lg-3 col-md-4 col-sm-6 single-filter-content content-5" data-myorder="5">
 					<div class="overlay overlay-bg-content d-flex align-items-center justify-content-center flex-column">
-						<p class="text-white">Media Coverage</p>
+						<p class="text-white">Barad Innovations</p>
 						<div class="line"></div>
-						<h5 class="text-white text-uppercase">Weddings & Events</h5>
+						<h5 class="text-white text-uppercase">We create lasting memories</h5>
 					</div>
 				</div>
 				<div class="mix category-1 category-3 category-4 col-lg-3 col-md-4 col-sm-6 single-filter-content content-6" data-myorder="6">
 					<div class="overlay overlay-bg-content d-flex align-items-center justify-content-center flex-column">
-						<p class="text-white">Media Coverage</p>
+						<p class="text-white">Barad Innovations</p>
 						<div class="line"></div>
-						<h5 class="text-white text-uppercase">Weddings & Events</h5>
+						<h5 class="text-white text-uppercase">We create lasting memories</h5>
 					</div>
 				</div>
-				<div class="mix category-2 category-4 category-3 col-lg-3 col-md-4 col-sm-6 single-filter-content content-7" data-myorder="7">
+				<div class="mix category-2 category-1 category-3 col-lg-3 col-md-4 col-sm-6 single-filter-content content-7" data-myorder="7">
 					<div class="overlay overlay-bg-content d-flex align-items-center justify-content-center flex-column">
-						<p class="text-white">Media Coverage</p>
+						<p class="text-white">Barad Innovations</p>
 						<div class="line"></div>
-						<h5 class="text-white text-uppercase">Weddings & Events</h5>
+						<h5 class="text-white text-uppercase">We create lasting memories</h5>
 					</div>
 				</div>
-				<div class="mix category-2 category-4 category-3 col-lg-3 col-md-4 col-sm-6 single-filter-content content-8" data-myorder="8">
+				<div class="mix category-2 category-1 category-4 col-lg-3 col-md-4 col-sm-6 single-filter-content content-8" data-myorder="8">
 					<div class="overlay overlay-bg-content d-flex align-items-center justify-content-center flex-column">
-						<p class="text-white">Media Coverage</p>
+						<p class="text-white">Barad Innovations</p>
 						<div class="line"></div>
-						<h5 class="text-white text-uppercase">Weddings & Events</h5>
+						<h5 class="text-white text-uppercase">We create lasting memories</h5>
 					</div>
 				</div>
 			</div>
